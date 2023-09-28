c119dc6bd5cdf154d4d7fa31ade400c262775ecd
lexgp <lexgp@yandex.ru>
2023-09-28 22:29:40 +0300

    Init Vite + Android App

---
+++ android/app/src/main/java/com/calcunyator3/MainActivity.java
@@ -0,0 +1,5 @@
+package com.calcunyator3;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.calcunyator3',
+  appName: 'calcunyator3',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
